51383f1994dbcabcb51c83c4faadf19966992804
Juan Andres <82684670+juan2175606@users.noreply.github.com>
2022-08-28 11:00:30 -0500

    Add files via upload

---
+++ src/tristate.v
@@ -0,0 +1,30 @@
+`default_nettype none
+`timescale 1ns/1ns
+`ifdef COCTB
+`include "/home/andres/asic_tools/pdk/sky130A/libs.ref/sky130_fd_sc_hd/verilog/primiives.v"
+`include "/home/andres/asic_tools/pdk/sky130A/libs.ref/sky130_fd_sc_hd/verilog/sky130_fd_sc_hd.v"
+`endif
+
+module tristate(
+    input  A   ,
+    input  TE_B,
+    output Z  
+    );
+
+wire OUT1;
+
+sky130_fd_sc_hd__ebufn tr1(
+    .Z(Z)   ,
+    .A (A)  ,
+    .TE_B(TE_B),
+    .VPWR(1'b1),
+    .VGND(1'b0),
+    .VPB(1'b0) ,
+    .VNB(1'b0)
+);
+
+
+
+
+
+endmodule
